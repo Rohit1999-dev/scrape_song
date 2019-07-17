f3ddf33dc36cb88a7973ec4e10f98518e917a95e
rohityo <44015004+rohityo@users.noreply.github.com>
2019-07-17 09:35:10 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -11,12 +11,16 @@ main_div=soup.find('ul',class_="content-container artworkload clearfix a-list")
 
 get_name=main_div.find_all('div',class_="arwtork_label")
 # print(get_)
-count=0
+
+		# Here i scraped songs type name as their text...............
+count=0			
 for i in get_name:
 	a_=i.find('a').get_text()
 	count+=1
 	print(count,a_)
 get_=main_div.find_all('div',class_="hover-events-parent")
+
+			# Here i scraped their link...........
 list_=[]
 for i in get_:
 	link=(i.find('a').get('href'))
@@ -50,6 +54,6 @@ for i in div:
 user1=int(input("enter the number of song:"))
 link = Url_list[user1-1]
 # print(link)
-webbrowser.open_new_tab(link)
+webbrowser.open_new_tab(link)  # Here Aplication used to access and view website
 
 
